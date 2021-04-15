4cf096984faa6eff5ddf4d0e36106db978117e86
mandeni <VIOLA991102@GMAIL.COM>
2021-04-15 11:17:38 +0200

    moves changes

--- app/src/main/java/com/example/lyton/ViewPagerAdapter.java
+++ app/src/main/java/com/example/lyton/ViewPagerAdapter.java
@@ -7,8 +7,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.example.lyton.ChatFragment;
-import com.example.lyton.PostFragment;
+import com.example.lyton.activity_fragment.ChatFragment;
+import com.example.lyton.activity_fragment.PostFragment;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
