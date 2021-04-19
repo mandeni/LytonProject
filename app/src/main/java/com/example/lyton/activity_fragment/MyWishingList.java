49f496ff181a83a0a7159e70227dddc7271a23c3
mandeni <VIOLA991102@GMAIL.COM>
2021-04-19 17:22:18 +0200

    spot mvvm

--- app/src/main/java/com/example/lyton/activity_fragment/MyWishingList.java
+++ app/src/main/java/com/example/lyton/activity_fragment/MyWishingList.java
@@ -2,21 +2,38 @@ package com.example.lyton.activity_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.lyton.R;
+import com.example.lyton.adapter.SpotAdapter;
+import com.example.lyton.model.Spot;
+import com.example.lyton.viewModel.PostViewModel;
+import com.example.lyton.viewModel.SpotViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyWishingList extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private SpotAdapter spotAdapter;
+
+
+    private List<Spot> spots = new ArrayList<>();
+    private SpotViewModel spotViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_wishing_list);
+
+        //      View Model
+        spotViewModel = new ViewModelProvider(this).get(SpotViewModel.class);
+        spotViewModel.getAllSpot().observe(this, spots -> spotAdapter.updateData(spots));
 
         //      Toolbar setting
         Toolbar toolBar = findViewById(R.id.toolbar_my_wishing_list);
